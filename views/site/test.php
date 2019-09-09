120dea46d93caf764a118e410d492b194d544f29
Oleg Rasskazov <info@olegrasskazov.pro>
2019-09-09 22:17:32 +0300

    Initial commit

---
+++ views/site/test.php
@@ -0,0 +1,18 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'New TestPage';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+	<ul>
+		<li>Item 1</li>
+		<li>Item 2</li>
+		<li>Item 3</li>
+	</ul>
+</div>
